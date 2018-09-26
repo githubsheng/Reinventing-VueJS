e885f98dc9ac1eb95eb9703501eef44843203edf
wang sheng <sheng.wang@rakuten.com>
2018-09-26 17:38:47 +0900

    wip

--- SV.js
+++ SV.js
@@ -1,8 +1,55 @@
 function SV(options){
-    const content = options.render.call(options.data);
-    const el = document.querySelector(options.el);
-    el.innerHTML = "";
-    el.appendChild(content);
+    this.options = options;
+    this.value = document.querySelector(options.el);
+    this.mount();
 }
 
+SV.prototype.evaluate = function(){
+    return this.options.render.call(this.options.data);
+};
 
+SV.prototype.update = function(newVal){
+    const parent = this.value.parentElement;
+    parent.insertBefore(newVal, this.value);
+    parent.removeChild(this.value);
+    this.value = newVal;
+};
+
+SV.prototype.mount = function(){
+    const newVal = this.evaluate();
+    this.update(newVal);
+};
+
+SV.
+
+
+SV.component("name", {
+    data: function(){
+        return {
+            firstName: "Darth",
+            lastName: "Vader"
+        }
+    },
+    render: function(){
+        const p = document.createElement("p");
+        p.innerText = `${this.firstName} ${this.lastName}`;
+        return p;
+    }
+});
+
+var vm = new SV({
+    el: '#app',
+    data: {
+        message: 'Greetings!'
+    },
+    render: function(){
+        const p = document.createElement("p");
+        p.innerText = this.message;
+
+        const name = useComponent("name");
+
+        const div = document.createElement("div");
+        div.appendChild(p);
+        div.appendChild(name);
+    }
+});
