7443f2a6177d6e6bf815d47dfdd76236dbab42b5
wang sheng <sheng.wang@rakuten.com>
2018-09-28 09:42:43 +0900

    use child component

--- SV.js
+++ SV.js
@@ -9,9 +9,11 @@ SV.prototype.evaluate = function(){
 };
 
 SV.prototype.update = function(newVal){
-    const parent = this.value.parentElement;
-    parent.insertBefore(newVal, this.value);
-    parent.removeChild(this.value);
+    if(this.value && this.value.parentElement) {
+        const parent = this.value.parentElement;
+        parent.insertBefore(newVal, this.value);
+        parent.removeChild(this.value);
+    }
     this.value = newVal;
 };
 
@@ -20,9 +22,22 @@ SV.prototype.mount = function(){
     this.update(newVal);
 };
 
-SV.
+SV.options = {
+    components: Object.create(null)
+};
 
+SV.component = function(name, definition) {
+    SV.options.components[name] = definition;
+};
 
+SV.useComponent = function(name){
+    const options = SV.options.components[name];
+    options.data = options.data();
+    const component = new SV(options);
+    return component.value;
+};
+
+//use
 SV.component("name", {
     data: function(){
         return {
@@ -37,7 +52,7 @@ SV.component("name", {
     }
 });
 
-var vm = new SV({
+const vm = new SV({
     el: '#app',
     data: {
         message: 'Greetings!'
@@ -46,10 +61,11 @@ var vm = new SV({
         const p = document.createElement("p");
         p.innerText = this.message;
 
-        const name = useComponent("name");
+        const name = SV.useComponent("name");
 
         const div = document.createElement("div");
         div.appendChild(p);
         div.appendChild(name);
+        return p;
     }
 });
