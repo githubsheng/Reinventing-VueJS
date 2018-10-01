8a9fbc9433c5ffe7921fe1c9cd410420ea54d638
wang sheng <sheng.wang@rakuten.com>
2018-10-01 14:02:01 +0900

    data proxy and method binding

--- SV.js
+++ SV.js
@@ -1,5 +1,7 @@
 function SV(options){
     this.options = options;
+    initData(this);
+    initMethods(this);
     const el = document.querySelector(options.el);
     if(el) {
         this.value = el;
@@ -8,7 +10,7 @@ function SV(options){
 }
 
 SV.prototype.evaluate = function(){
-    return this.options.render.call(this.options.data);
+    return this.options.render.call(this);
 };
 
 SV.prototype.patch = function(newVal){
@@ -52,14 +54,65 @@ SV.component("name", {
     }
 });
 
+function initData(vm){
+    let data = vm.options.data;
+    if(typeof data === 'function') {
+        data = data();
+    }
+    const keys = Object.keys(data);
+    let i = keys.length;
+    while(i--) {
+       const key = keys[i];
+       if(isReserved(key)) continue;
+       /*
+        for each property in `options.data`,
+        we create a pair of getters and setters on our component
+        instance `vm`.
+        */
+       Object.defineProperty(vm, key, {
+           configurable: true,
+           enumerable: true,
+           get: function proxyGetter(){
+                return vm.options.data[key];
+           },
+           set: function proxySetter(val){
+               vm.options.data[key] = val;
+           }
+       })
+    }
+}
+
+function initMethods(vm) {
+    const methods = vm.options.methods;
+    if(methods) {
+        for(let methodName in methods) {
+            const method = methods[methodName];
+            vm[methodName] = function(){
+                return method.apply(vm, arguments);
+            }
+        }
+    }
+}
+
+function isReserved (str) {
+    var c = (str + '').charCodeAt(0)
+    return c === 0x24 || c === 0x5F
+}
+
 const vm = new SV({
     el: '#app',
     data: {
         message: 'Greetings!'
     },
+    methods: {
+        printMessage: function(){
+            console.log(this.message);
+        }
+    },
     render: function(){
         const p = document.createElement("p");
         p.innerText = this.message;
+        p.addEventListener("click", this.printMessage);
 
         const name = SV.useComponent("name");
 
